834bc83a72ea1df9c6b45800ddac85a0b31a74d5
Danielateresa <dany.t_87@hotmail.it>
2023-01-19 16:21:22 +0100

    Print data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
     components: {
-        HelloWorld,
+
+    },
+    data() {
+        return {
+            projects: null,
+            api_url: 'http://localhost:8000',
+            loading: true,
+            error: null
+
+        }
+    },
+    methods: {
+        //chiamata ajax per ottenere tutti i post del db
+        getProjects(url) {
+            axios.get(url)
+                .then(response => {
+                    console.log(response.data.results);
+                    this.projects = response.data.results;
+                    this.loading = false;
+
+
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.error = error.message
+                    this.loading = false;
+
+
+                });
+        }
+    },
+    mounted() {
+        this.getProjects(this.api_url + '/api/projects');
     }
 }
 </script>
@@ -11,17 +44,14 @@ export default {
 <template>
     <section class="vue-home">
         <div class="container">
+            <h1>All Projects</h1>
+
             <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
-                    <div class="logos">
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
+                <div class="col-3" v-for="project in projects.data">
+                    <img class="card-image" src="" alt="">
+                    <div class="card-body">
+                        <h3>{{ project.title }}</h3>
                     </div>
-                    <HelloWorld />
                 </div>
             </div>
         </div>
@@ -30,31 +60,4 @@ export default {
 
 <style lang="scss">
 @use './styles/general.scss';
-
-.vue-home {
-    color: #2c3e50;
-    background: #181818;
-    transition: color 0.5s, background-color 0.5s;
-    line-height: 1.6;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
