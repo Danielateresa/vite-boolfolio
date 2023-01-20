ba1f86fcb83b7fa3d879c53eeb5c9d467137c4df
Danielateresa <dany.t_87@hotmail.it>
2023-01-20 18:42:20 +0100

    Add 404

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'NotFoundView'
+}
+
+</script>
+
+<template>
+    <div class="container pt-3 text-center pt-5">
+        <h1>404 - Page not found</h1>
+        <router-link to="/">Return to Home</router-link>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+img {
+    width: 100%;
+}
+</style>
