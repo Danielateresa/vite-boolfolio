436f340b363760c8146680cfe89b0fe727f8422e
Danielateresa <dany.t_87@hotmail.it>
2023-01-26 14:11:51 +0100

    Add Footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,70 @@
+<script>
+
+export default {
+    name: 'AppFooter',
+}
+</script>
+
+
+<template>
+
+    <footer class="pb-3">
+        <div class="container social py-5">
+
+            <h3 class="text-center text-light">Follow me on...</h3>
+            <div class="text-center py-5 d-flex justify-content-center gap-5">
+                <div class="square d-flex align-items-center justify-content-center">
+                    <a href="https://www.linkedin.com/in/daniela-teresa-de-leo-5b506122a/" target="_blank">
+                        <font-awesome-icon icon="fa-brands fa-linkedin-in" size="2x" />
+                    </a>
+                </div>
+                <div class="square d-flex align-items-center justify-content-center">
+                    <a href="https://github.com/Danielateresa" target="_blank">
+                        <font-awesome-icon icon="fa-brands fa-github" size="2x" />
+                    </a>
+                </div>
+
+            </div>
+
+            <p class="w-75 m-auto">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, dolore
+                corrupti!
+                Molestias,
+                ipsum
+                magnam. Corrupti nobis at iure et repudiandae explicabo, iste in magnam maiores. Veritatis voluptas
+                culpa, exercitationem amet inventore nobis.</p>
+        </div>
+        <div class="container bottom_footer text-center">
+            <hr class="text-white">
+            <small>&copy; Daniela Teresa de Leo | 2023</small>
+        </div>
+    </footer>
+
+</template>
+
+
+<style lang="scss" scoped>
+footer {
+    box-shadow: 0 5px 5px 0 #7c6d91 inset;
+    background-color: #9c89b6;
+
+    a {
+        color: white;
+
+        .icon {
+            width: 150%;
+        }
+    }
+
+    .square {
+        width: 50px;
+        aspect-ratio: 1/1;
+        border-radius: 30%;
+        box-shadow: 0 2px 5px 0 #7c6d91;
+
+        &:hover {
+            box-shadow: 0 1px 1px 0 #7c6d91;
+            transform: scale(99%);
+        }
+    }
+}
+</style>
