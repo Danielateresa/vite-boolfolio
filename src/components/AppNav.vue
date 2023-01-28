3ddcf5736f29bc71452c5ae428e317ea68a7814d
Danielateresa <dany.t_87@hotmail.it>
2023-01-28 22:07:15 +0100

    Fix margin

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -17,8 +17,8 @@ export default {
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse bg-dark bg-opacity-75 rounded mt-4" id="collapsibleNavId">
-                <ul class="navbar-nav text-uppercase text-end ms-auto mt-2 mt-lg-0">
+            <div class="collapse navbar-collapse bg-dark bg-opacity-50 rounded mt-2" id="collapsibleNavId">
+                <ul class="navbar-nav text-uppercase text-end ms-auto mt-lg-0">
                     <li class="nav-item">
                         <router-link class="nav-link" :to="{ name: 'home' }" aria-current="page">Home <span
                                 class="visually-hidden">(current)</span></router-link>
